80fd4f2d18d0d7ae8e37365ebf8c9fa1060209df
Olanrewaju <lawallanre.abbahouse@gmail.com>
2025-03-14 16:22:38 +0100

    teat

--- app/main.py
+++ app/main.py
@@ -40,7 +40,7 @@ origins = [
 app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*", "http://localhost:3000", "http://127.0.0.1:3000", "http://127.0.0.1:8000", "http://localhost:8000"],
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"], 
