f6624132dd9580e6e66075f768663d90b5d44183
Olanrewaju <lawallanre.abbahouse@gmail.com>
2025-03-18 20:29:17 +0100

    done

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 
 if __name__ == "__main__":
     ENV = os.getenv("ENV", "development")
-    HOST = "0.0.0.0" if ENV == "production" else "127.0.0.1"
+    HOST = "0.0.0.0"
     uvicorn.run(
         app="app.main:app", 
         host=HOST, 
