22e6294e5c5390d63968b6985c043cf3d7f6e964
Olanrewaju <lawallanre.abbahouse@gmail.com>
2025-03-18 20:24:55 +0100

    done

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import uvicorn
-
 import os
 
 if __name__ == "__main__":
     ENV = os.getenv("ENV", "development")
     HOST = "0.0.0.0" if ENV == "production" else "127.0.0.1"
-    PORT = int(os.getenv("PORT", 8000)) 
     uvicorn.run(
-        "app.main:app", 
+        app="app.main:app", 
         host=HOST, 
         port=8000, 
-        reload=(ENV == "development")
+        reload=True,
     )
