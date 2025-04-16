a83f8bb26276a53fdfa698d00471fe9316d19568
Olanrewaju <lawallanre.abbahouse@gmail.com>
2025-04-16 19:45:37 +0100

    updated the google auth signin

--- src/users/routes.py
+++ src/users/routes.py
@@ -301,6 +301,11 @@ async def validate(request: Request):
             url=frontend_redirect_url,
             headers={
                 "Set-Cookie": f"access_token={access_token}; Path=/; HttpOnly; SameSite=None; Max-Age=18000",
+                "Location": frontend_redirect_url,
+                "Access-Control-Allow-Origin": "http://localhost:3000",
+                "Access-Control-Allow-Credentials": "true",
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
             },
             status_code=302,
         )
